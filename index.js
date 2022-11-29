b2c5bc2ad3aa2815cebc75e4ba730cfe1e092fc0
Mohan0884 <106987098+Mohan0884@users.noreply.github.com>
2022-11-29 20:21:33 +0530

    Initial

---
+++ index.js
@@ -0,0 +1,52 @@
+function scrolll(){
+    var left=document.querySelector('.report-scroll-images');
+    left.scrollBy(300,0);
+}
+function scrollr(){
+    var right=document.querySelector('.report-scroll-images');
+    right.scrollBy(-300,0);
+}
+function handlesidebar(){
+    var x=document.getElementById('sidebar');
+    if(x.style.display==="none"){
+        x.style.display="flex";
+    }else{
+        x.style.display="none";
+    }
+}
+function func1(){
+    var a=document.getElementById('func1');
+    if(a.style.display==="none"){
+        a.style.display="flex";
+    }else{
+        a.style.display="none";
+
+    }
+}
+function func2(){
+    var b=document.getElementById('func2');
+    if(b.style.display==="none"){
+        b.style.display="flex";
+    }else{
+        b.style.display="none";
+
+    }
+}
+function func3(){
+    var c=document.getElementById('func3');
+    if(c.style.display==="none"){
+        c.style.display="flex";
+    }else{
+        c.style.display="none";
+
+    }
+}
+function func4(){
+    var d=document.getElementById('func4');
+    if(d.style.display==="none"){
+        d.style.display="flex";
+    }else{
+        d.style.display="none";
+
+    }
+}
